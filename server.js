c123cc18fc91a2c089022975a8db8ed039b7c30b
hmzugur <craxx67@gmail.com>
2021-05-05 00:36:16 +0300

    Partial pages & Html page

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-
-
-
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const path = require('path')
+
+
+dotenv.config( { path:'config.env'} )
+const PORT = process.env.PORT || 8080
+
+// log request
+app.use(morgan('tiny'))
+
+// body-parser depracated therefore  express
+app.use(express.urlencoded({ extended : true }))
+
+// set view engine & EJS 
+app.set("view engine","ejs")
+//app.set("views", path.resolve(__dirname,"views/ejs"))
+
+// ALL static files
+app.use(express.static('public'))
 
 
 app.get('/',(req,res) => {
 
-    res.send('CRUD Application Hello ....')
+    res.render('index')
 })
 
-app.listen(3000,() => {
-    console.log(`Server is runnig http://localhost:${3000}`);
+app.listen(PORT,() => {
+    console.log(`Server is runnig http://localhost:${PORT}`);
 })
