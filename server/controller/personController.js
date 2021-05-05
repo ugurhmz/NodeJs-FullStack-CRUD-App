b248085a353a13feb63b11105e51b6cb7b0ba0d9
hmzugur <craxx67@gmail.com>
2021-05-05 21:11:05 +0300

    Usage POSTMAN user create

--- server/controller/personController.js
+++ server/controller/personController.js
@@ -5,6 +5,34 @@ const Person = require('../model/personModel')
 // create and save() USER
 exports.create = (req,res) => {
 
+    // validate request
+    if(!req.body){
+        res.status(400).send( { message : 'Content can not be empty!'} )
+        return;
+    }
+
+    //+New User
+    const user = new  Person({
+        name :  req.body.name,
+        email:  req.body.email,
+        gender: req.body.gender,
+        status: req.body.status
+    })
+
+    // save user in DB
+
+    user.save(user)
+        .then(data => {
+            console.log(data)
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Error!"
+            })
+        })
+
+
 }
 
 
@@ -21,5 +49,5 @@ exports.update = (req,res) => {
 
 // delete User with ID
 exports.delete = (req,res) => {
-    
+
 }
