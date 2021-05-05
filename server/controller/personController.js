061cefeb861a20226518e72a94dc3403e88e5a5d
hmzugur <craxx67@gmail.com>
2021-05-05 20:00:51 +0300

    Created API/Routes

---
+++ server/controller/personController.js
@@ -0,0 +1,25 @@
+
+const Person = require('../model/personModel')
+
+
+// create and save() USER
+exports.create = (req,res) => {
+
+}
+
+
+// Get all Users
+exports.read = ( req,res) => {
+
+}
+
+
+// update User with ID
+exports.update = (req,res) => {
+
+}
+
+// delete User with ID
+exports.delete = (req,res) => {
+    
+}
--- server/routes/allRoutes.js
+++ server/routes/allRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const services = require('../services/render')
-
+const personController = require('../controller/personController')
 
 
 router.get('/',services.indexRoutes)
 router.get('/add-user',services.add_user)
 router.get('/update-user', services.update_user)
+
+
+
+//API
+router.post('/api/users', personController.create)
+router.get('/api/users', personController.read)
+router.put('/api/users/:id', personController.update)
+router.delete('/api/users/:id', personController.delete)
 
 
 
