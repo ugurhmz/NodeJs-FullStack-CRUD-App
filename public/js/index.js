e15f516753d262e7b772882343dda9683b0c5ffb
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-05-07 04:58:47 +0300

    The Project Done

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,11 @@
-
+//_____________________________ CREATE __________________________________
 $('#add_user').submit(function (e) {
-
-    $('.addMessage').html('<strong>User added successfully</strong>')
+   
+    $('.addMessage').text('User added successfully')
     
 })
 
-
+//___________________________________ UPDATE ____________________
 $('#update_user').submit(function (e)  {
     e.preventDefault()
 
@@ -33,3 +33,35 @@ $('#update_user').submit(function (e)  {
     })
 
 })
+
+
+//___________________________________ DELETE ____________________
+if(window.location.pathname =='/'){
+
+    $ondelete = $(".table tbody td a.delete")
+    $ondelete.click(function (e) {
+        var id = $(this).attr("data-id")
+
+
+        var request = {
+            "url":`http://localhost:3000/api/users/${id}`,
+            "method":"DELETE"
+        }
+
+        //_______________ confirm_____________
+        if(confirm("Do you wanna delete this user?? ")){
+            $.ajax(request).done(function(response) {
+                
+                
+                    $('.addMessage').text('User DELETED successfully!')
+                    setTimeout(function(){
+                        location.reload()
+                    },500 )
+            
+            })
+        
+        }
+      
+    })
+
+}
